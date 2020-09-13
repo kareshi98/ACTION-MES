6d7d3f5f827468a9b81a3fe81b354a0cc5566d73
kareshi98 <kareshi98@foxmail.com>
2020-09-13 14:16:50 +0800

    质量管理

--- src/cn/action/modules/tec/service/ProcessStationService.java
+++ src/cn/action/modules/tec/service/ProcessStationService.java
@@ -1,14 +1,33 @@
 package cn.action.modules.tec.service;
 
+
+import cn.action.common.service.CrudService;
+import cn.action.modules.bas.entity.WorkStationInfos;
+import cn.action.modules.tec.dao.ProcessStationDao;
+import cn.action.modules.tec.entity.ProcessStation;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import cn.action.common.service.CrudService;
-import cn.action.modules.tec.dao.ProcessStationDao;
-import cn.action.modules.tec.entity.ProcessStation;
+import java.util.List;
 
 @Service
 @Transactional(readOnly=true)
 public class ProcessStationService extends CrudService<ProcessStationDao, ProcessStation>{
-
+    /**
+     * 根据工站对象获得对应的工序对象
+     * @param workStationInfos
+     * @return
+     */
+    public ProcessStation findProcessStationByStation(WorkStationInfos workStationInfos) {
+        ProcessStation processStation=new ProcessStation();
+        processStation.setWorkStationInfos(workStationInfos);
+        List<ProcessStation> list=this.findList(processStation);
+        if(list==null) {
+            return null;
+        }
+        if(list.size()>0) {
+            return list.get(0);
+        }
+        return null;
+    }
 }
